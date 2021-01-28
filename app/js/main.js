0dd770dccbf5276c19327169af0cebd9e44fe3f4
Alexey-544 <544-lexa@mail.ru>
2021-01-28 16:45:38 +0400

    added categories section

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,21 @@ $(function () {
 $('.blog__items').slick({
     dots: true,
     arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+
+  var mixer1 = mixitup('.product-content__gallery', {
+selectors: {
+  control: '.filter-1',
+}
+  });
+
+  var mixer2 = mixitup('.new-design__content', {
+selectors: {
+  control: '.filter-2',
+}
   });
 
 
